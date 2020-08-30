9c7ef7822768602328938dd282e91fa2496a9f04
Boris Melikian <boris@lariushin.org>
2020-08-30 16:15:20 +0300

    External URLs update.

--- src/App.vue
+++ src/App.vue
@@ -2,58 +2,58 @@
   <div id="app">
     <img width="300" src="./assets/logo.png">
     <div class="links">
-      &bull;
-      <a class="link" href="https://algaerium.info/">algaerium.info</a> &bull;
-      <a class="link" href="https://antropology.tv/">antropology.tv</a> &bull;
-      <a class="link" href="https://asmodeus.vip/">asmodeus.vip</a> &bull;
-      <br />
-      &bull;
-      <a class="link" href="https://bettycat.vip/">bettycat.vip</a> &bull;
-      <a class="link" href="https://www.bibliothecium.info/"
-        >bibliothecium.info</a
-      >
-      &bull;
-      <a class="link" href="https://centralbrainz.tv/">centralbrainz.tv</a>
-      &bull;
-      <br />
-      &bull;
-      <a class="link" href="https://egypt-trip.cc/">egypt-trip.cc</a> &bull;
-      <a class="link" href="https://eldritch-horror.com/"
-        >eldritch-horror.com</a
-      >
-      &bull;
-      <a class="link" href="https://fungarium.info/">fungarium.info</a> &bull;
-      <br />
-      &bull;
-      <a class="link" href="https://herbarium.info/">herbarium.info</a> &bull;
-      <a class="link" href="https://horror-movies.best/">horror-movies.best</a>
-      &bull;
-      <a class="link" href="https://iceland-trip.cc/">iceland-trip.cc</a> &bull;
-      <br />
-      &bull;
-      <a class="link" href="https://karelia-trip.cc/">karelia-trip.cc</a> &bull;
-      <a class="link" href="https://käpalised.com/">käpalised.com</a> &bull;
-      <a class="link" href="https://lariushin.com/">lariushin.com</a> &bull;
-      <br />
-      &bull;
-      <a class="link" href="https://lichenarium.info/">lichenarium.info</a>
-      <a class="link" href="https://melikian.vip/">melikian.vip</a> &bull;
-      <a class="link" href="https://nightshade.blog/">nightshade.blog</a> &bull;
-      <br />
-      &bull;
-      <a class="link" href="https://öland-saar.cc/">öland-saar.cc</a> &bull;
-      <a class="link" href="https://plantarium.info/">plantarium.info</a> &bull;
-      <a class="link" href="https://sciencebooks.cc/">sciencebooks.cc</a> &bull;
-      <br />
-      &bull;
-      <a class="link" href="https://sochi-trip.cc/">sochi-trip.cc</a> &bull;
-      <a class="link" href="https://tenerife-trip.cc/">tenerife-trip.cc</a>
-      &bull;
-      <a class="link" href="https://travel-pics.vip/">travel-pics.vip</a> &bull;
-      <br />
-      &bull;
-      <a class="link" href="https://wheeloftheyear.page/">wheeloftheyear.page</a
-      > &bull;<br />
+    &bull;
+    <a class="link" href="https://algaerium.info/">algaerium.info</a> &bull;
+    <a class="link" href="https://antropology.tv/">antropology.tv</a> &bull;
+    <a class="link" href="https://asmodeus.vip/">asmodeus.vip</a> &bull;
+    <br />
+    &bull;
+    <a class="link" href="https://bettycat.vip/">bettycat.vip</a> &bull;
+    <a class="link" href="https://www.bibliothecium.info/"
+      >bibliothecium.info</a
+    >
+    &bull;
+    <a class="link" href="https://centralbrainz.tv/">centralbrainz.tv</a>
+    &bull;
+    <br />
+    &bull;
+    <a class="link" href="https://egypt-trip.cc/">egypt-trip.cc</a> &bull;
+    <a class="link" href="https://eldritch-horror.com/"
+      >eldritch-horror.com</a
+    >
+    &bull;
+    <a class="link" href="https://fungarium.info/">fungarium.info</a> &bull;
+    <br />
+    &bull;
+    <a class="link" href="https://herbarium.info/">herbarium.info</a> &bull;
+    <a class="link" href="https://horror-movies.best/">horror-movies.best</a>
+    &bull;
+    <a class="link" href="https://iceland-trip.cc/">iceland-trip.cc</a> &bull;
+    <br />
+    &bull;
+    <a class="link" href="https://karelia-trip.cc/">karelia-trip.cc</a> &bull;
+    <a class="link" href="https://käpalised.com/">käpalised.com</a> &bull;
+    <a class="link" href="https://lariushin.com/">lariushin.com</a> &bull;
+    <br />
+    &bull;
+    <a class="link" href="https://lichenarium.info/">lichenarium.info</a> &bull;
+    <a class="link" href="https://melikian.vip/">melikian.vip</a> &bull;
+    <a class="link" href="https://nightshade.blog/">nightshade.blog</a> &bull;
+    <br />
+    &bull;
+    <a class="link" href="https://öland-saar.cc/">öland-saar.cc</a> &bull;
+    <a class="link" href="https://plantarium.info/">plantarium.info</a> &bull;
+    <a class="link" href="https://sciencebooks.cc/">sciencebooks.cc</a> &bull;
+    <br />
+    &bull;
+    <a class="link" href="https://sochi-trip.cc/">sochi-trip.cc</a> &bull;
+    <a class="link" href="https://tenerife-trip.cc/">tenerife-trip.cc</a>
+    &bull;
+    <a class="link" href="https://travel-pics.vip/">travel-pics.vip</a> &bull;
+    <br />
+    &bull;
+    <a class="link" href="https://wheeloftheyear.page/">wheeloftheyear.page</a
+    > &bull; <br />
     </div>
     <router-view/>
   </div>
